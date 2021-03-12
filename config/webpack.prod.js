26ac6810f9200a242bfec10be171b3ea2769e44a
Pratap Dessai <pratap.dessai@MBP-C02Z95D3LVDL.leboncoin.loc>
2021-03-12 16:24:39 +0100

    docs(Setup): updated setup files

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -15,6 +15,7 @@ const prodConfig = {
   output: {
     path: path.resolve(__dirname, "../dist"),
     filename: "[name].[contenthash].js",
+    publicPath: "dist",
   },
   plugins: [
     new ModuleFederationPlugin({
